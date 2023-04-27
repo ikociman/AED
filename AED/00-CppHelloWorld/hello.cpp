daf5de24307bfb62f7ac9ac72c6d7520e0b1bb8a
ikociman <132012914+ikociman@users.noreply.github.com>
2023-04-27 18:29:38 -0300

    Add files via upload

---
+++ AED/00-CppHelloWorld/hello.cpp
@@ -0,0 +1,13 @@
+/* Hello World
+ * Lineas de codigo que imprimen en la pantalla "Hello World" con un salto de linea
+ * Kociman Ivan Jose
+ * 20230527
+ */
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World \n";
+}
